05c4bb4e33a4f5f3bb84b03a362bd02a2aa37398
brittanymbui <brittymbui@gmail.com>
2021-08-18 16:09:24 -0700

    Adds photos

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -2,30 +2,29 @@ import React, { useState, useEffect } from 'react';
 import FlowerCard from './FlowerCard';
 
 const Content = () => {
-    const [flowers, setFlowers] = useState('');
+    const [flowers, setFlowers] = useState([]);
 
     useEffect(() => {
         const url = 'https://dtcqvoap3l.execute-api.us-west-1.amazonaws.com/default/interviewTakeHomeApi'
 
         const fetchData = async() => {
-            try {
                 const response = await fetch(url);
                 const data = await response.json();
                 console.log(data);
                 setFlowers(data);
-            } catch(error) {
-                console.log(error);
-            }
         }
 
         fetchData();
     }, []);
 
+    const listFlowers = flowers.map(flower => {
+        return <FlowerCard flower={flower} key={flower.id} />
+    })
+
         return (
             <div className="content">
                 <h1>Flowers</h1>
-                {flowers.map(flower =>
-                    <FlowerCard key={flower.id} flower={flower} />)}
+                { flowers && listFlowers }
             </div>
         )
 }
--- src/Content/FlowerCard.js
+++ src/Content/FlowerCard.js
@@ -2,6 +2,7 @@
 const FlowerCard = ({ flower }) => {
     return (
         <div>
+            <img src={flower.img_url} alt="flower" />
             <h3>{flower.name}</h3>
         </div>
     )
